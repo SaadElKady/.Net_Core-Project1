b56a4abaf02053bfb0cb66dad9fba0fda508a186
SaadElKady <saadelkady45@gmail.com>
2021-11-10 21:36:45 +0200

    message

---
+++ ProjectV1/Controllers/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectV1.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult ViewShop()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectV1/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectV1.Models
+{
+    public class Item
+    {
+        public long Id{ get; set; }
+        [Display (Name = "Item Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        [Display (Name = "2000 EGP")]
+        public string Price{ get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
